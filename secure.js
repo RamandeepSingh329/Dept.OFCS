43ed10be330cc342801966fb696cfaa8242b32bf
Ramandeep Singh <codersingh94@gmail.com>
2025-09-02 13:50:09 +0530

    update

--- secure.js
+++ secure.js
@@ -14,3 +14,21 @@ document.addEventListener("contextmenu",(e)=>{
     e.preventDefault();
     // alert("Right click is isabled!")
 })
+document.addEventListener("keydown", function(e) {
+      // F12
+      if (e.key === "F12") {
+        e.preventDefault();
+      }
+      // F11 (fullscreen)
+      if (e.key === "F11") {
+        e.preventDefault();
+      }
+      // Ctrl+Shift+I
+      if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") {
+        e.preventDefault();
+      }
+      // Ctrl+Shift+J (console)
+      if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "j") {
+        e.preventDefault();
+      }
+      });
